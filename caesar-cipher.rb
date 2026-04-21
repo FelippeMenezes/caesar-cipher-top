278c7437ad1408dfc4c5291dcf09191cacac77a2
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-04-21 11:59:42 -0300

    Rubocop auto correct

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,14 +1,11 @@
 def caesar_cipher(string, number)
   transformed = string.chars.map do |char|
-
     if char.match?(/[a-z]/)
       base = 'a'.ord
       position = char.ord - base
       new_position = position + number
 
-      while new_position >= 26
-        new_position -= 26
-      end
+      new_position -= 26 while new_position >= 26
 
       (new_position + base).chr
 
@@ -17,26 +14,23 @@ def caesar_cipher(string, number)
       position = char.ord - base
       new_position = position + number
 
-      while new_position >= 26
-        new_position -= 26
-      end
+      new_position -= 26 while new_position >= 26
 
       (new_position + base).chr
 
     else
       char
     end
-
   end
 
   result = transformed.join
   p result
 end
 
-p "What phrase do you want to encode?"
+p 'What phrase do you want to encode?'
 user_string = gets.chomp
 
-p "What is the secret number of your code?"
+p 'What is the secret number of your code?'
 secret_number = gets.chomp.to_i
 
 caesar_cipher(user_string, secret_number)
